b5f8dd5252fe10576baa31756a5283f9685524f7
UrielGarmendia <garmendiauriel@gmail.com>
2023-10-26 15:31:35 -0300

    desafío 1

--- Hooks/1. Show/Hide title/src/App.jsx
+++ Hooks/1. Show/Hide title/src/App.jsx
@@ -1,18 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [hide, setHide] = useState(false);
-
-  const handleClick = () => {
-    setHide(!hide);
-  };
-
-  return (
-    <>
-      <button onClick={handleClick}>Show/Hide</button>
-      {hide === true ? <h1>Hola</h1> : null}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
