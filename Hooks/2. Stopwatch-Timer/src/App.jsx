933d0893f11bd0f352916182c361190b6d4a138e
UrielGarmendia <garmendiauriel@gmail.com>
2023-10-26 20:07:40 -0300

    desafio 2 listo

--- Hooks/2. Stopwatch-Timer/src/App.jsx
+++ Hooks/2. Stopwatch-Timer/src/App.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+
 function App() {
-  return <></>;
+  const [timer, setTimer] = useState(0);
+
+  const handleStart = () => {
+    window.myTimer = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+  const handleStop = () => {
+    clearInterval(window.myTimer);
+  };
+
+  const handleReset = () => {
+    clearInterval(window.myTimer);
+    setTimer(0);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>Timer</h1>
+        <span>{Math.trunc(timer / 60)} mins </span>
+        <span>{timer % 60} secs</span>
+        <div className="buttons">
+          <button onClick={handleStart}>Start</button>
+          <button onClick={handleStop}>Stop</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
